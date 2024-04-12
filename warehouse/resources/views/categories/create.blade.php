7b465c7a52a4be3d4367e3f2f3e2301a10d8dc1a
issaevis <evis.topollaj@gmail.com>
2024-04-12 11:24:14 +0200

    Removed unnecessary files and did some refactoring

--- warehouse/resources/views/categories/create.blade.php
+++ warehouse/resources/views/categories/create.blade.php
@@ -1,26 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <title>Posts</title>
-</head>
-
-<body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-warning">
-        <div class="container-fluid">
-            <a class="navbar-brand h1" href="{{ route('categories.index') }}">CRUDcategories</a>
-            <div class="justify-end ">
-                <div class="col ">
-                    <a class="btn btn-sm btn-success" href="{{ route('categories.create') }}">Add Post</a>
-                </div>
-            </div>
-        </div>
-    </nav>
-
+@section('content')
     <div class="container h-100 mt-5">
         <div class="row h-100 justify-content-center align-items-center">
             <div class="col-10 col-md-8 col-lg-6">
@@ -37,7 +17,4 @@
             </div>
         </div>
     </div>
-
-</body>
-
-</html>
+@stop
--- warehouse/resources/views/categories/edit.blade.php
+++ warehouse/resources/views/categories/edit.blade.php
@@ -1,29 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-  <title>Categories</title>
-</head>
-<body>
-  <nav class="navbar navbar-expand-lg navbar-light bg-warning">
-    <div class="container-fluid">
-      <a class="navbar-brand h1" href="{{ route('categories.index') }}">CRUDcategories</a>
-      <div class="justify-end ">
-        <div class="col ">
-          <a class="btn btn-sm btn-success" href="{{ route('categories.create') }}">Add Post</a>
-        </div>
-      </div>
-    </div>
-  </nav>
+@extends('layouts.app')
 
-  <div class="container h-100 mt-5">
-  <div class="row h-100 justify-content-center align-items-center">
-    <div class="col-10 col-md-8 col-lg-6">
-      <h3>Update Category</h3>
+@section('content')
+    <div class="container h-100 mt-5">
+        <div class="row h-100 justify-content-center align-items-center">
+            <div class="col-10 col-md-8 col-lg-6">
+                <h3>Update Category</h3>
       <form action="{{ route('categories.update', $category->id) }}" method="post">
         @csrf
         @method('PUT')
@@ -32,12 +13,10 @@ integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6
           <input type="text" class="form-control" id="title" name="title"
             value="{{ $category->title }}" required>
         </div>
-        
+
         <button type="submit" class="btn mt-3 btn-primary">Update Category</button>
       </form>
     </div>
   </div>
 </div>
-
-</body>
-</html>
+@stop
