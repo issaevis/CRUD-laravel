9e64dbfb41e9bc63205c367a382f3a8ec1df3514
issaevis <evis.topollaj@gmail.com>
2024-04-16 14:46:24 +0200

    Forgot to push this while editing the products. Adding it now

--- warehouse/routes/web.php
+++ warehouse/routes/web.php
@@ -2,16 +2,15 @@
 
 use App\Http\Controllers\CategoriesController;
 use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
 Route::middleware('auth')->group(function () {
+    Route::get('/', [HomeController::class, 'index'])->name('dashboard');
     Route::resource('categories', CategoriesController::class);
     Route::resource('products', ProductsController::class);
-    Route::get('/', function () {
-        return view('dashboard');
-    })->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
